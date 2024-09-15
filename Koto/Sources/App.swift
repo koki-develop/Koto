de1b139c1318101dbf387b2a693ad60b019cfc9f
koki-develop <kou.pg.0131@gmail.com>
2024-09-15 22:27:07 +0900

    Implement NSApplication

--- Koto/Sources/App.swift
+++ Koto/Sources/App.swift
@@ -5,4 +5,29 @@
 //  Created by koki sato on 2024/09/15.
 //
 
-import Foundation
+import InputMethodKit
+
+class NSManualApplication: NSApplication {
+    let appDelegate = AppDelegate()
+
+    override init() {
+        super.init()
+        self.delegate = self.appDelegate
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var server = IMKServer()
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        self.server = IMKServer(
+            name: Bundle.main.infoDictionary?["InputMethodConnectionName"] as? String,
+            bundleIdentifier: Bundle.main.bundleIdentifier
+        )
+    }
+}
