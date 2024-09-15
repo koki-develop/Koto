165724f872c3ee3cd5dad789f14f01319d9ea6e1
koki-develop <kou.pg.0131@gmail.com>
2024-09-15 22:16:03 +0900

    Initialize project

---
+++ Koto/KotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KotoApp.swift
+//  Koto
+//
+//  Created by koki sato on 2024/09/15.
+//
+
+import SwiftUI
+
+@main
+struct KotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
