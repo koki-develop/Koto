9e6d3f5098127fab8fe9dbc44b876d92ea4a0f45
koki-develop <kou.pg.0131@gmail.com>
2024-09-16 14:37:02 +0900

    Implement getEventType

--- Koto/Sources/EventType.swift
+++ Koto/Sources/EventType.swift
@@ -5,4 +5,25 @@
 //  Created by koki sato on 2024/09/16.
 //
 
-import Foundation
+import AppKit
+
+enum EventType {
+    case input(_ text: String)
+}
+
+func getEventType(_ event: NSEvent) -> EventType? {
+    if let text = event.characters, isPrintable(text) {
+        return .input(text)
+    }
+
+    return nil
+}
+
+private func isPrintable(_ text: String) -> Bool {
+    let printable = [
+        CharacterSet.alphanumerics,
+        CharacterSet.symbols,
+        CharacterSet.punctuationCharacters,
+    ].reduce(CharacterSet()) { $0.union($1) }
+    return !text.unicodeScalars.contains(where: { !printable.contains($0) })
+}
