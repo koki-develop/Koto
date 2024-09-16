baa3a191c306c348766204505c6b15f0dcfd58f3
koki-develop <kou.pg.0131@gmail.com>
2024-09-16 14:38:42 +0900

    Override init

--- Koto/Sources/InputController.swift
+++ Koto/Sources/InputController.swift
@@ -9,6 +9,9 @@ import InputMethodKit
 
 @objc(KotoInputController)
 class KotoInputController: IMKInputController {
+  override init!(server: IMKServer!, delegate: Any!, client inputClient: Any!) {
+    super.init(server: server, delegate: delegate, client: inputClient)
+  }
 
   override func handle(_ event: NSEvent!, client sender: Any!) -> Bool {
     return false
