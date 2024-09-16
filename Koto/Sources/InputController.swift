c26e0c317f57faf3c56fe77171d2231324bc674d
koki-develop <kou.pg.0131@gmail.com>
2024-09-16 22:21:21 +0900

    Implement composing text

--- Koto/Sources/InputController.swift
+++ Koto/Sources/InputController.swift
@@ -6,16 +6,55 @@
 //
 
 import InputMethodKit
+import KanaKanjiConverterModule
 
 @objc(KotoInputController)
 class KotoInputController: IMKInputController {
-  private let inputSubject 
+  var state: InputState = .normal
+  var composingText: ComposingText = ComposingText()
 
   override init!(server: IMKServer!, delegate: Any!, client inputClient: Any!) {
     super.init(server: server, delegate: delegate, client: inputClient)
   }
 
   override func handle(_ event: NSEvent!, client sender: Any!) -> Bool {
-    return false
+    guard let eventType = getEventType(event) else {
+      return false
+    }
+
+    switch (eventType, self.state) {
+    case (.input(let text), .normal):
+      self.state = .composing
+      fallthrough
+    case (.input(let text), .composing):
+      self.composingText.insertAtCursorPosition(text, inputStyle: .roman2kana)
+      self.setMarkedText(self.composingText.convertTarget)
+      return true
+
+    case (.enter, .composing):
+      self.insertText(self.composingText.convertTarget)
+      self.setMarkedText("")
+      self.state = .normal
+      self.composingText = ComposingText()
+      return true
+
+    default:
+      return false
+    }
+  }
+
+  private func insertText(_ text: String) {
+    guard let client = self.client() else {
+      return
+    }
+    client.insertText(text, replacementRange: NSRange(location: NSNotFound, length: NSNotFound))
+  }
+
+  private func setMarkedText(_ text: String) {
+    guard let client = self.client() else {
+      return
+    }
+    let range = NSRange(location: NSNotFound, length: NSNotFound)
+    client.setMarkedText(text, selectionRange: range, replacementRange: range)
   }
 }
