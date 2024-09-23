6a0f52fd3dd7c2d262c8949dd5bf99019145a434
koki-develop <kou.pg.0131@gmail.com>
2024-09-24 08:15:37 +0900

    Add KeyCodes struct

--- Koto/Sources/KeyCodes.swift
+++ Koto/Sources/KeyCodes.swift
@@ -5,4 +5,21 @@
 //  Created by koki sato on 2024/09/24.
 //
 
-import Foundation
+// ref: https://gist.github.com/swillits/df648e87016772c7f7e5dbed2b345066
+struct Keycodes {
+  static let yen: UInt16 = 0x5d
+  static let enter: UInt16 = 0x24
+  static let space: UInt16 = 0x31
+  static let backspace: UInt16 = 0x33
+  static let escape: UInt16 = 0x35
+
+  static let leftArrow: UInt16 = 0x7B
+  static let rightArrow: UInt16 = 0x7C
+  static let downArrow: UInt16 = 0x7D
+  static let upArrow: UInt16 = 0x7E
+
+  static let h: UInt16 = 0x04
+  static let k: UInt16 = 0x28
+  static let n: UInt16 = 0x2D
+  static let p: UInt16 = 0x23
+}
