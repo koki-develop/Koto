7bebba50e2c315082bbd3f875a6cae01eb0844bf
koki-develop <kou.pg.0131@gmail.com>
2024-09-24 08:20:04 +0900

    Implement isPrintable

--- Koto/Sources/Utils.swift
+++ Koto/Sources/Utils.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+func isPrintable(_ text: String) -> Bool {
+  let printable = [
+    CharacterSet.alphanumerics,
+    CharacterSet.symbols,
+    CharacterSet.punctuationCharacters,
+  ].reduce(CharacterSet()) { $0.union($1) }
+  return !text.unicodeScalars.contains(where: { !printable.contains($0) })
+}
