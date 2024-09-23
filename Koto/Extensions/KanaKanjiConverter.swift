f994e35668b640e5f5dce4e4337fb9ed4e8db4db
koki-develop <kou.pg.0131@gmail.com>
2024-09-23 21:48:03 +0900

    Implement convenience init and convert to KanaKanjiConverter

--- Koto/Extensions/KanaKanjiConverter.swift
+++ Koto/Extensions/KanaKanjiConverter.swift
@@ -7,7 +7,29 @@
 
 import KanaKanjiConverterModuleWithDefaultDictionary
 
+private var convertOptions: ConvertRequestOptions {
+  .withDefaultDictionary(
+    requireJapanesePrediction: false,
+    requireEnglishPrediction: false,
+    keyboardLanguage: .ja_JP,
+    learningType: .inputAndOutput,
+    memoryDirectoryURL: .cachesDirectory,
+    sharedContainerURL: .cachesDirectory,
+    zenzaiMode: .off,
+    metadata: .init()
+  )
+}
+
 extension KanaKanjiConverter {
+  convenience init() {
+    let dicdataStore = DicdataStore(convertRequestOptions: convertOptions)
+    self.init(dicdataStore: dicdataStore)
+  }
+
+  func convert(_ composingText: ComposingText) -> ConversionResult {
+    return self.requestCandidates(composingText, options: convertOptions)
+  }
+
   func saveLearningData() {
     self.sendToDicdataStore(.closeKeyboard)
   }
